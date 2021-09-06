455a5cc295ab5199204fa5ce249c1152128731e2
DimochKaaa <zaaz288@mail.ru>
2021-09-06 06:53:42 +0300

    updated comments

--- persistence/src/main/java/com/epam/esm/dao/creator/criteria/Criteria.java
+++ persistence/src/main/java/com/epam/esm/dao/creator/criteria/Criteria.java
@@ -8,11 +8,16 @@ package com.epam.esm.dao.creator.criteria;
 public interface Criteria {
 
     /**
-     * Add search or sort criteria to query string.
+     * Adds search or sort criteria to query string.
      *
      * @param query current query
      */
     void addCriteria(StringBuilder query);
 
+    /**
+     * Gets criteria field name
+     *
+     * @return String criteria field name
+     */
     String getCriteriaField();
 }
