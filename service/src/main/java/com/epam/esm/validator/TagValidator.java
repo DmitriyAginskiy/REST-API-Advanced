1220c083f6d673b36661da40ac36d762ba8d50ad
DimochKaaa <zaaz288@mail.ru>
2021-09-07 15:54:56 +0300

    remove component annotation

--- service/src/main/java/com/epam/esm/validator/TagValidator.java
+++ service/src/main/java/com/epam/esm/validator/TagValidator.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
  * @author Dzmitry Ahinski
  */
 public class TagValidator {
+
     private static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z0-9\\w\\s]{1,50}");
 
     public static boolean isNameValid(String name) {
