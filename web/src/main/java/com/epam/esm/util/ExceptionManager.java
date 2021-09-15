2eaa8028036b53893c5b0cb70747fa9a87243ab4
DimochKaaa <zaaz288@mail.ru>
2021-09-16 02:48:39 +0300

    create class

---
+++ web/src/main/java/com/epam/esm/util/ExceptionManager.java
@@ -0,0 +1,43 @@
+package com.epam.esm.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Manages exceptions of web layer.
+ *
+ * @author Dzmitry Ahinski
+ */
+public enum ExceptionManager {
+
+    INVALID_PARAMETER(40002, "invalid_parameter"),
+    INVALID_FIELD(40003, "invalid_field"),
+    UNSUPPORTED_MEDIA_TYPE(41501, "unsupported_media_type");
+
+    private static final String BUNDLE_PATH = "global_message";
+
+    private int code;
+    private String message;
+
+    ExceptionManager(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage(Object... objectFields) {
+        String localMessage = new String(ResourceBundle
+                .getBundle(BUNDLE_PATH, Locale.getDefault())
+                .getString(message)
+                .getBytes(StandardCharsets.ISO_8859_1));
+        if(objectFields.length != 0) {
+            localMessage += Arrays.toString(objectFields);
+        }
+        return localMessage;
+    }
+}
